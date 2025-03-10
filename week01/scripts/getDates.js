b6c7aec3573f04458fa32f017bca31e16a8bea22
marastac <132803403+marastac@users.noreply.github.com>
2025-03-10 15:37:29 -0500

    week-1

---
+++ week01/scripts/getDates.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Año actual en el footer
+    document.getElementById("year").textContent = new Date().getFullYear();
+
+    // Última modificación de la página
+    document.getElementById("lastModified").textContent = "Last Modification: 01/01/1970 12:00:00";
+});
